9dcadbae2c44ef5a74e6bf883c70550915390044
sEybers <167661614+sEybers@users.noreply.github.com>
2024-12-11 08:06:47 -0500

    added js for login and register forms

--- js/main.js
+++ js/main.js
@@ -1,9 +1,47 @@
-console.log("Hello World!")
+class Product{
+    constructor(id,name,price){
+        this.productID=id;
+        this.productName=name;
+        this.productPrice=price;
+    }
 
-let random = ["pizza",9,false,4.56]
+    //getter methods
+    getProductID(){
+        return this.productID;
+    }
+    getProductName(){
+        return this.productName;
+    }
+    getProductPrice(){
+        return this.productPrice;
+    }
+    //setter methods
 
-for (let i = 0; i < random.length; i++) {
-
-    console.log(random[i])
+    setProductName(id) {
+        this.productID = id;
+    }
+    setProductName(name){
+        this.productName = name;
+    }
+    setProductPrice(price){
+        this.productPrice = price;
+    }
 }
+    const p1 = new Product(001,"Yoyo",5);
+    console.log(p1);
+    p1.setProductPrice(8)
+    console.log(p1);
+
+
+let findSum = (i1,i2,i3) => (i1+i2+i3);
+
+// let findAverage = (i1,i2,i3) => {
+//     let sum = findSum(i1,i2,i3)
+//     return sum/3
+// }
+
+let findAverage = (i1,i2,i3) => (findSum(i1,i2,i3))/3;
+
+console.log(findAverage(5,7,9));
+
 
